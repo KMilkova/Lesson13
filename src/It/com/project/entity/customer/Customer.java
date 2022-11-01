6c5fcf973432b1db7d7bfb0a5f0561a7dc117382
KMilkova <58460336+KMilkova@users.noreply.github.com>
2022-11-01 19:57:25 +0300

    Update Customer.java

--- src/It/com/project/entity/customer/Customer.java
+++ src/It/com/project/entity/customer/Customer.java
@@ -88,5 +88,25 @@ public class Customer {
 				+ ", address = " + address + ", creditCardNumber = " + creditCardNumber + ", bankAccountNumber = "
 				+ bankAccountNumber;
 	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, bankAccountNumber, creditCardNumber, ind, middleName, name, surname);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		return Objects.equals(address, other.address) && Objects.equals(bankAccountNumber, other.bankAccountNumber)
+				&& creditCardNumber == other.creditCardNumber && ind == other.ind
+				&& Objects.equals(middleName, other.middleName) && Objects.equals(name, other.name)
+				&& Objects.equals(surname, other.surname);
+	}
 
 }
